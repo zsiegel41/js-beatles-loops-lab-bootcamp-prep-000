7e97db7eacd80555224e2987608fd8778bc61f55
Zach Siegel <zachsiegel41@gmail.com>
2017-09-17 17:36:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0, i<musicians.length, i++) {
+    array.push("${musicians} plays ${instruments}.")
+  }
+}
+
+function johnLennonFacts() {
+
+}
